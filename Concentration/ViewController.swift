85b142a33af74c1baba8d75cc7005e802e28f1dc
Carlos Gongora <cgongoram@hotmail.com>
2019-01-25 20:32:45 -0500

    Conversion to xcode 10

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -41,10 +41,10 @@ class ViewController: UIViewController
         
         print("flipCard(withEmoji: \(emoji))")
         if button.currentTitle == emoji {
-            button.setTitle("", for: UIControlState.normal)
+            button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         } else {
-            button.setTitle(emoji, for: UIControlState.normal)
+            button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         }
         
